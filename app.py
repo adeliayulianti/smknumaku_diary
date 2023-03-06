f59647053be851cb0e59106eb839e2711f1f192e
unknown <adelia yulianti>
2023-03-06 12:14:53 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
+import certifi
+ca = certifi.where()
 
 #connection_string = 'mongodb+srv://admin:admin@cluster0.pjbjofm.mongodb.net/?retryWrites=true&w=majority'
 #client = MongoClient(connection_string)
@@ -17,7 +19,6 @@ MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME =  os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
-
 db = client[DB_NAME]
 
 
@@ -34,8 +35,8 @@ def show_diary():
 
 @app.route('/diary', methods=['POST'])
 def save_diary():
-    title = request.form.get('title')
-    content = request.form.get('content')
+    title_receive = request.form.get('title_give')
+    content_receive = request.form.get('content_give')
 
     today = datetime.now()
     mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
@@ -50,14 +51,17 @@ def save_diary():
     profilename = f'static/profile-{mytime}.{extension}'
     profile.save(profilename)
 
+    time = today.strftime('%Y.%m.%d')
+
     doc = {
         'file' : filename,
         'profile': profilename,
-        'title': title,
-        'content': content
+        'title': title_receive,
+        'content': content_receive,
+        'time' : time,
     }
     db.diary.insert_one(doc)
-    return jsonify({'message': 'data was saved!'})
 
+    return jsonify({'message': 'data was saved!'})
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
